026c3f0cf2b43a8fb6fa99a889f4d7a3bcd72a8e
OzdemirOsman <osman200181@gmail.com>
2021-11-19 11:24:10 -0500

    Osman's first c

--- src/test/java/MyClass1.java
+++ src/test/java/MyClass1.java
@@ -2,5 +2,7 @@ public class MyClass1 {
 
     public static void main(String[] args) {
         System.out.println("Set up line");
+        String deneme = "Deneme";
+        System.out.println(deneme);
     }
 }
