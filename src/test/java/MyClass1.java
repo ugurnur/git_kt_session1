cacaeeec1127c390b2e902bdc00f269c20536340
Cuneyt <june8kc@gmail.com>
2021-11-19 20:09:29 +0300

    second commit

--- src/test/java/MyClass1.java
+++ src/test/java/MyClass1.java
@@ -3,6 +3,7 @@ import org.w3c.dom.ls.LSOutput;
 public class MyClass1 {
     public static void main(String[] args) {
         System.out.println("Cuneyt line");
+        System.out.println("Cuneyt line 2");
         System.out.println("Set up line");
     }
 
